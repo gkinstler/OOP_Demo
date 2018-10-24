fd8bd3f123e07437055cd35a1f3b311c77d3a313
Glenn Kinstler <glenn@glennkinstler.com>
2018-10-24 13:56:08 -0500

    first commit - abstraction and inheritance

--- OOP-Demo/Program.cs
+++ OOP-Demo/Program.cs
@@ -2,8 +2,43 @@
 
 namespace OOP_Demo
 {
-    class Program
+    // This is an example of Abstraction.  In my example, I'm not letting the user name a standalone Sandwich
+
+    class Sandwiches
     {
+        public string Name { get; set; }
+        public bool HasBread { get; set; }
+        public bool HasSomethingInTheMiddle { get; set; }
+        public int NumberOfCondiments { get; set; }
+
+    }
+
+    // This is an example of Inheritance
+
+    class Hotdog : Sandwiches
+    {
+        public bool HasTubeOfMeat { get; set; }
+    }
+
+    // This is an example of Encapsulation
+
+    private string CreateHotDog(Hotdog hotdog)
+    {
+        
+    }
+
+    //  This is an example of Polymorphism
+
+    
+}
+
+
+
+
+
+
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
---
+++ OOP-Demo/Sandwiches.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Demo
+{
+    class Sandwiches
+    {
+        public bool HasBread { get; set; }
+        public bool HasSomethingInTheMiddle { get; set; }
+
+    }
+
+ 
+}
